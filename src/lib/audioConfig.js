57e7eaf15028004f32091e8d7e624a391b059f27
PaddlePOPA96 <dutahad@gmail.com>
2026-05-24 21:07:25 +0700

    add bahlil

--- src/lib/audioConfig.js
+++ src/lib/audioConfig.js
@@ -1,10 +1,9 @@
 export const audioOptions = [
   { label: "upin ipin", value: "/sounds/tamatlah-sudah.mp3" },
   { label: "IKEVE YAMAL", value: "/sounds/ikeve.mp3" },
+  { label: "Bahlil", value: "/sounds/bahlil-song.mp3" },
   { label: "Goal Horn", value: "/sounds/goal.mp3" },
   { label: "GGMU", value: "/sounds/ggmu.mp3" },
-  { label: "Parado no Bailao", value: "/sounds/parado-no-bailao.mp3" },
-  { label: "Lamine Yamal", value: "/sounds/lamine-yamal.mp3" },
   { label: "antek-antek", value: "/sounds/antek.mp3" },
   { label: "jokowi", value: "/sounds/jokowi.mp3" },
   { label: "dj-kicau-mania", value: "/sounds/dj-kicau-mania.mp3" },
